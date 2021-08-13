68468039b99491ceea83ef77bce88fc50a66dd11
En-Chi Liu <enchi.liu@gmail.com>
2021-08-13 23:44:11 +0200

    import CardSection, ProjectSection

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@
 import "./styles/style.css";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
+import CardSection from "./components/CardSection";
+import ProjectSection from "./components/ProjectSection";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Intro />
+      <CardSection sectionHeading="Skills" />
+      <ProjectSection />
     </div>
   );
 }
