daf523ccdec0fde6826cf8eae6a6abea64909d22
En-Chi Liu <enchi.liu@gmail.com>
2021-08-13 22:04:13 +0200

    create Intro

---
+++ src/components/Intro.tsx
@@ -0,0 +1,18 @@
+// Project files
+import profilePicture from "../assets/images/profile-pic.jpg";
+
+export default function Intro() {
+  return (
+    <section>
+      <h1>Hi, I'm En-Chi</h1>
+      <img src={profilePicture} alt="En-Chi headshot" />
+      <p>
+        Hej Hej! I'm a Taiwanese-Brit now based in Sweden and training as a
+        software developer thanks to KTH and Novare Potential. I previously
+        worked with digital communications and direct marketing at a top global
+        university in Scotland. My past colleagues would tell you that I'm a
+        positive team player with a passion for lifelong learning.
+      </p>
+    </section>
+  );
+}
