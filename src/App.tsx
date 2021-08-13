5e7cb1b79e4e47b6d76a95cbcbbf9caf589ae874
En-Chi Liu <enchi.liu@gmail.com>
2021-08-13 22:04:26 +0200

    import Header and Intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 // Project files
 import "./styles/style.css";
+import Header from "./components/Header";
+import Intro from "./components/Intro";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Intro />
+    </div>
+  );
 }
 
 export default App;
