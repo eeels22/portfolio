8731c741f1424c0e7320cd88e4a163024ee927f2
En-Chi Liu <enchi.liu@gmail.com>
2021-08-14 00:24:36 +0200

    Create Contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+export default function Contact() {
+  return (
+    <div>
+      <h2>Contact me</h2>
+      <p>
+        Interested in my work? Connect with me on Linkedin or send me an email!
+      </p>
+      <ul>
+        <li>LinkedIn</li>
+        <li>Email</li>
+      </ul>
+    </div>
+  );
+}
