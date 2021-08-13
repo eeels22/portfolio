9cc3b563b01ca24a34322a344731dc8fad45a7d1
En-Chi Liu <enchi.liu@gmail.com>
2021-08-14 00:06:34 +0200

    conditionally initialise cardArray

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -1,12 +1,21 @@
 // Project files
 import SkillSetCard from "./SkillSetCard";
 import skillsData from "../data/skillsData.json";
+import ActivityCard from "./ActivityCard";
+import activityData from "../data/activityData.json";
 
 export default function CardSection({ sectionHeading }) {
   // Components
-  const cardArray = skillsData.skillSets.map((element) => (
-    <SkillSetCard key={element.id} data={element} />
-  ));
+  let cardArray;
+  if (sectionHeading === "Skills") {
+    cardArray = skillsData.skillSets.map((element) => (
+      <SkillSetCard key={element.id} data={element} />
+    ));
+  } else if (sectionHeading === "Work and education") {
+    cardArray = activityData.activities.map((element) => (
+      <ActivityCard key={element.id} data={element} />
+    ));
+  }
 
   return (
     <section>
