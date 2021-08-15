13d012cf9106f6cab2bd9acfba1b39a3a4a06ed9
En-Chi Liu <enchi.liu@gmail.com>
2021-08-15 20:48:19 +0200

    add dynamic class for ActivityCard

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -14,7 +14,7 @@ interface ActivityCardProps {
 
 export default function ActivityCard({ data }: ActivityCardProps) {
   return (
-    <div className="ActivityCard card">
+    <div className={`ActivityCard card ${data.category}`}>
       <h3>{data.title}</h3>
       <p>{data.location}</p>
       <p>
