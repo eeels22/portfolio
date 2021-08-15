0a3fa9113d5b8ebfd365946b95a8ba5c65d1db42
En-Chi Liu <enchi.liu@gmail.com>
2021-08-15 21:32:32 +0200

    style Intro

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -4,15 +4,19 @@ import profilePicture from "../assets/images/profile-pic.jpg";
 export default function Intro() {
   return (
     <section className="Intro">
-      <h1>Hi, I'm En-Chi</h1>
       <img src={profilePicture} alt="En-Chi headshot" className="profile-pic" />
-      <p>
-        Hej Hej! I'm a Taiwanese-Brit now based in Sweden and training as a
-        software developer thanks to KTH and Novare Potential. I previously
-        worked with digital communications and direct marketing at a top global
-        university in Scotland. My past colleagues would tell you that I'm a
-        positive team player with a passion for lifelong learning.
-      </p>
+      <div>
+        <h1>
+          Hi, I'm <span className="name">En-Chi</span>
+        </h1>
+        <p>
+          Hej Hej! I'm a Taiwanese-Brit now based in Sweden and training as a
+          software developer thanks to KTH and Novare Potential. I previously
+          worked with digital communications and direct marketing at a top
+          global university in Scotland. My past colleagues would tell you that
+          I'm a positive team player with a passion for lifelong learning.
+        </p>
+      </div>
     </section>
   );
 }
